dc5f29bad333caa1854886bdfaa0664dee4b1989
zhangshuo <zhangshuoabby@163.com>
2020-04-09 00:39:47 +0800

    accesstoken

--- src/main/java/com/example/demo/controllor/OAuthController.java
+++ src/main/java/com/example/demo/controllor/OAuthController.java
@@ -1,14 +1,27 @@
 package com.example.demo.controllor;
 
+import com.example.demo.dto.AccessTokenDTO;
+import com.example.demo.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OAuthController {
+    @Autowired
+    private GithubProvider githubProvider;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClien_id("8f7662daa403740303e6");
+        accessTokenDTO.setClient_secret("9067b5e3629c10450ce4d148f3caee9ee2211828");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri("https://localhost:8887/callback");
+        accessTokenDTO.setState(state);
+        githubProvider.getAccessToken(accessTokenDTO);
         return "index";
     }
 }
