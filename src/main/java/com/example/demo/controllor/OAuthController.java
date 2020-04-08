c83b66163e214270515f7e07086b64194a2e2ff8
zhangshuo <zhangshuoabby@163.com>
2020-04-08 23:14:46 +0800

    okhttp

---
+++ src/main/java/com/example/demo/controllor/OAuthController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controllor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OAuthController {
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state){
+        return "index";
+    }
+}
